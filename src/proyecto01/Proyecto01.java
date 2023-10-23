a0a52a95ee8bff2d3fc46aee448098fb657351c6
AbrahamCR19 <125339320+AbrahamCR19@users.noreply.github.com>
2023-10-22 18:54:10 -0600

    Update Proyecto01.java
    Se añadió un menú de opciones con todos los parametros

--- src/proyecto01/Proyecto01.java
+++ src/proyecto01/Proyecto01.java
@@ -1,16 +1,45 @@
 package proyecto01;
 
-    
+import javax.swing.JOptionPane;
+
 
 public class Proyecto01 {
-
     public static void main(String[] args) {
-        Usuario u = new Usuario();
-        u.agregarU();
-        u.agregarU();
-        u.consultarU();
-        u.consultarUEspecifico();
+        Usuario usuario = new Usuario();
+
+        while (true) {
+            String opcion = JOptionPane.showInputDialog(
+                    "Menú de Opciones:\n" +
+                    "1. Agregar Usuario\n" +
+                    "2. Consultar Usuarios\n" +
+                    "3. Consultar Usuario Específico\n" +
+                    "4. Inactivar Usuario\n" +
+                    "5. Salir\n" +
+                    "Ingrese el número de la opción deseada:"
+            );
+
+            switch (opcion) {
+                case "1":
+                    usuario.agregarU();
+                    break;
+                case "2":
+                    usuario.consultarU();
+                    break;
+                case "3":
+                    usuario.consultarUEspecifico();
+                    break;
+                case "4":
+                    usuario.inactivarU();
+                    break;
+                case "5":
+                    JOptionPane.showMessageDialog(null, "Saliendo del programa.");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida. Ingrese un número del 1 al 5.");
+                    break;
+            }
+        }
     }
-    //Fin
     
 }
